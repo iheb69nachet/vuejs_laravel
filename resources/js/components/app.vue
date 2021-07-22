9599f6b461723def5abf5a426aa466033adcba54
iheb nachet <inachet3@gmail.Com>
2021-07-22 23:36:12 +0100

    starter kit for vuejs laravel

---
+++ resources/js/components/app.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="container">
+        wiouww
+    </div>
+</template>
+
+<script>
+    export default {
+     
+    }
+</script>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        
+    </head>
+    <body class="antialiased">
+     <div id="app">
+         <app></app>
+</div>
+    </body>
+    <script src='{{mix("js/app.js")}}'>
+   
+    </script>
+</html>
